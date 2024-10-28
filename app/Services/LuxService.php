99f1e0eb68042cca788312718810a8cdbadbf967
Lennard <168357+lennardv2@users.noreply.github.com>
2024-10-28 18:00:11 +0100

    Variable component path using subdir config

--- app/Services/LuxService.php
+++ app/Services/LuxService.php
@@ -2,8 +2,6 @@
 
 namespace App\Services;
 
-use TailwindMerge\TailwindMerge;
-
 /**
  * WARNING DO NOT CHANGE: This files gets overwritten on every lux:publish
  * 
@@ -11,40 +9,8 @@ use TailwindMerge\TailwindMerge;
  */
 class LuxService
 {
-    public array $context = [];
-    public array $classList = [];
-    
-    public function __construct()
+    public function componentPath($component = '')  
     {
-    }
-
-    public function newContext(array $context) {
-        $this->context = $context;
-
-        return $this;
-    }
-
-    public function addClass(...$classList) {
-        $this->classList = array_merge($this->classList, $classList);
-
-        return $this;
-    }
-
-    public function addProperty($property, $classList, $default = 'default') {
-        $key = $this->context[$property] ?? $default;
-
-        $this->classList[] = $classList[$key] ?? $classList[$default];
-
-        return $this;
-    }
-
-    public function toClassString(...$classList) {
-        $this->addclass(...$classList);
-    
-        return join(' ', $this->classList);
-    }
-
-    public function __toString() {
-        return $this->toClassString();
+        return config('lux.subdir', '') . ($component ? '.' . $component : '');
     }
 }
