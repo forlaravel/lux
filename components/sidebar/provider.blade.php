e4f8fbd2abea81e639fe397546820c18834508ac
Lennard <168357+lennardv2@users.noreply.github.com>
2025-09-22 19:45:52 +0000

    wip

--- components/sidebar/provider.blade.php
+++ components/sidebar/provider.blade.php
@@ -7,8 +7,6 @@
 
 @open($tag)
     data-lux="sidebar.provider"
-    x-bind:data-open="sidebarOpen ? 'true' : 'false'"
-    x-bind:data-fixed="sidebarFixed ? 'true' : 'false'"
     x-cloak
     x-data="{
         sidebarOpen: @wireOr($open, handlePersist: true),
@@ -32,12 +30,32 @@
             requestAnimationFrame(() => {
                 this.$el.setAttribute('data-initialized', 'true');
             });
+            
+            // Add click event listener for backdrop
+            this.$el.addEventListener('click', (event) => this.handleBackdropClick(event));
         },
         toggleSidebar() {
             this.sidebarOpen = !this.sidebarOpen;
            
             if (this.sidebarFixed) {
                 this.updateBodyClasses();
+            }
+        },
+        closeSidebar() {
+            this.sidebarOpen = false;
+            
+            if (this.sidebarFixed) {
+                this.updateBodyClasses();
+            }
+        },
+        handleBackdropClick(event) {
+            // Check if click is on the backdrop (::before pseudo element area)
+            const sidebar = this.$el.querySelector('[data-lux=\'sidebar\']');
+            const trigger = event.target.closest('[data-lux=\'sidebar.trigger\']');
+            
+            // Don't close if clicking on trigger button or inside sidebar
+            if (!trigger && sidebar && !sidebar.contains(event.target) && window.innerWidth < 768) {
+                this.closeSidebar();
             }
         },
         updateBodyClasses() {
@@ -52,6 +70,8 @@
             }
         }
     }"
+    x-bind:data-open="sidebarOpen ? 'true' : 'false'"
+    x-bind:data-fixed="sidebarFixed ? 'true' : 'false'"
     @toggle-sidebar.window="toggleSidebar()"
 @content
     {{ $slot }}
